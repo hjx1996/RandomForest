f1b1fcce4401f519ab3cf51cf786e491b520dc1e
Purie <nahjx1996@163.com>
2019-11-16 05:30:30 +0800

    Undone New add RandomForestRegressor

--- RandomForestRegress.py
+++ RandomForestRegress.py
@@ -1,8 +1,27 @@
 # !/usr/bin/python3
 # encoding: utf-8
 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import pandas as pd
 import numpy as np
 
 data_set = pd.read_csv('GreenView.csv')
-data_set.head()
+# data_set.head()
+
+x = data_set.iloc[:, 1:7].values
+y = data_set.iloc[:, 0].values
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+regressor = RandomForestRegressor(n_estimators=200, random_state=0)
+regressor.fit(x_train, y_train)
+y_pred = regressor.predict(x_test)
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+# print(confusion_matrix(y_test,y_pred))
+# print(classification_report(y_test,y_pred))
+# print(accuracy_score(y_test, y_pred))
